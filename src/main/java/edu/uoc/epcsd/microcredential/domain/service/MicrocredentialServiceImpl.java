5d2890cdab3898d8622778ac9a408dc6b8fe18ae
juanmafe <juanreyes.1990@gmail.com>
2026-05-17 12:02:09 +0200

    - Fixes in api and services according to architecture model

--- src/main/java/edu/uoc/epcsd/microcredential/domain/service/MicrocredentialServiceImpl.java
+++ src/main/java/edu/uoc/epcsd/microcredential/domain/service/MicrocredentialServiceImpl.java
@@ -3,59 +3,111 @@ package edu.uoc.epcsd.microcredential.domain.service;
 import edu.uoc.epcsd.microcredential.domain.Microcredential;
 import edu.uoc.epcsd.microcredential.domain.ports.in.MicrocredentialPort;
 import edu.uoc.epcsd.microcredential.domain.ports.out.MicrocredentialPersistencePort;
+import edu.uoc.epcsd.microcredential.infrastructure.kafka.KafkaConstants;
 import edu.uoc.epcsd.microcredential.infrastructure.kafka.MicrocredentialMessage;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
-@RequiredArgsConstructor
-@Service
+/** The type Microcredential service impl. */
+@Slf4j
 @Validated
+@Service
+@RequiredArgsConstructor
 public class MicrocredentialServiceImpl implements MicrocredentialPort {
 
   private final MicrocredentialPersistencePort microcredentialPersistencePort;
   private final KafkaTemplate<String, MicrocredentialMessage> microcredentialKafkaTemplate;
 
-  @Value("${courseService.url}")
-  private String courseServiceUrl;
-
   @Override
-  public Optional<Microcredential> getMicrocredentialById(Long microcredentialId) {
-    // TODO: Complete the implementation
-    return null;
+  public Optional<Microcredential> getById(@NotNull final Long microcredentialId) {
+    return this.microcredentialPersistencePort.getById(microcredentialId);
   }
 
   @Override
-  public Microcredential requestMicrocredential(Microcredential microcredential) {
-    // TODO: Complete the implementation
-    return null;
+  public Microcredential createMicrocredential(@NotNull final Microcredential microcredential) {
+
+    final var microcredentialSaved = this.microcredentialPersistencePort.save(microcredential);
+
+    final var microcredentialMessage =
+        MicrocredentialMessage.builder()
+            .microcredentialId(microcredentialSaved.id())
+            .enrollment(microcredentialSaved.enrollmentId())
+            .userEmail(null)
+            .courseId(null)
+            .build();
+
+    this.microcredentialKafkaTemplate.send(
+        KafkaConstants.MICROCREDENTIAL_TOPIC + KafkaConstants.SEPARATOR + KafkaConstants.PENDING,
+        microcredentialMessage);
+
+    log.info(
+        "Microcredential {} requested for enrollment {}",
+        microcredentialSaved.id(),
+        microcredentialSaved.enrollmentId());
+
+    return microcredentialSaved;
   }
 
   @Override
-  public Microcredential approvePendingMicrocredential(Long microcredentialId) {
-    // TODO: Complete the implementation
-    return null;
+  public Microcredential approvePendingMicrocredential(@NotNull final Long microcredentialId) {
+
+    final var microcredentialApproved =
+        this.microcredentialPersistencePort.updateStatus(microcredentialId, Boolean.TRUE);
+
+    final var microcredentialMessage =
+        MicrocredentialMessage.builder()
+            .microcredentialId(microcredentialApproved.id())
+            .enrollment(microcredentialApproved.enrollmentId())
+            .userEmail(null)
+            .courseId(null)
+            .build();
+
+    this.microcredentialKafkaTemplate.send(
+        KafkaConstants.MICROCREDENTIAL_TOPIC + KafkaConstants.SEPARATOR + KafkaConstants.APPROVED,
+        microcredentialMessage);
+
+    log.info(
+        "Microcredential {} approved for enrollment {}",
+        microcredentialApproved.id(),
+        microcredentialApproved.enrollmentId());
+
+    return microcredentialApproved;
   }
 
   @Override
-  public Microcredential rejectPendingMicrocredential(Long microcredentialId) {
-    // TODO: Complete the implementation
-    return null;
-  }
+  public Microcredential rejectPendingMicrocredential(@NotNull final Long microcredentialId) {
 
-  @Override
-  public List<Microcredential> getCourseMicrocredentials(Long courseId) {
-    // TODO: Complete the implementation
-    return null;
+    final var microcredentialRejected =
+        this.microcredentialPersistencePort.updateStatus(microcredentialId, Boolean.FALSE);
+
+    final var microcredentialMessage =
+        MicrocredentialMessage.builder()
+            .microcredentialId(microcredentialRejected.id())
+            .enrollment(microcredentialRejected.enrollmentId())
+            .userEmail(null)
+            .courseId(null)
+            .build();
+
+    this.microcredentialKafkaTemplate.send(
+        KafkaConstants.MICROCREDENTIAL_TOPIC + KafkaConstants.SEPARATOR + KafkaConstants.REJECTED,
+        microcredentialMessage);
+
+    log.info(
+        "Microcredential {} rejected for enrollment {}",
+        microcredentialRejected.id(),
+        microcredentialRejected.enrollmentId());
+
+    return microcredentialRejected;
   }
 
   @Override
   public List<Microcredential> getPendingMicrocredentials() {
-    // TODO: Complete the implementation
-    return null;
+    return this.microcredentialPersistencePort.findPending();
   }
 }
--- src/main/java/edu/uoc/epcsd/microcredential/infrastructure/repository/MicrocredentialRepository.java
+++ src/main/java/edu/uoc/epcsd/microcredential/infrastructure/repository/MicrocredentialRepository.java
@@ -1,10 +1,16 @@
 package edu.uoc.epcsd.microcredential.infrastructure.repository;
 
 import edu.uoc.epcsd.microcredential.domain.Microcredential;
+import edu.uoc.epcsd.microcredential.domain.MicrocredentialStatus;
+import edu.uoc.epcsd.microcredential.domain.exception.MicrocredentialNotFoundException;
 import edu.uoc.epcsd.microcredential.domain.ports.out.MicrocredentialPersistencePort;
+import edu.uoc.epcsd.microcredential.infrastructure.repository.mappers.MicrocredentialMapper;
 import edu.uoc.epcsd.microcredential.infrastructure.repository.springdata.MicrocredentialSpringData;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,34 +20,51 @@ import org.springframework.stereotype.Component;
 public class MicrocredentialRepository implements MicrocredentialPersistencePort {
 
   private final MicrocredentialSpringData microcredentialSpringData;
+  private final MicrocredentialMapper microcredentialMapper;
 
   @Override
-  public Optional<Microcredential> getById(Long microcredentialId) {
-    // TODO: Complete the implementation
-    return null;
+  public Optional<Microcredential> getById(@NotNull final Long microcredentialId) {
+
+    return this.microcredentialSpringData
+        .getMicrocredentialById(microcredentialId)
+        .map(this.microcredentialMapper::toDomain);
   }
 
   @Override
-  public Microcredential save(Microcredential microcredential) {
-    // TODO: Complete the implementation
-    return null;
+  public Microcredential save(@NotNull final Microcredential microcredential) {
+
+    final var microcredentialEntityToPersist = this.microcredentialMapper.toEntity(microcredential);
+    final var microcredentialEntity =
+        this.microcredentialSpringData.save(microcredentialEntityToPersist);
+    return this.microcredentialMapper.toDomain(microcredentialEntity);
   }
 
   @Override
-  public Microcredential updateStatus(Long microcredentialId, boolean approved) {
-    // TODO: Complete the implementation
-    return null;
+  public Microcredential updateStatus(
+      @NotNull final Long microcredentialId, final boolean approved) {
+
+    final var microcredentialEntity =
+        this.microcredentialSpringData
+            .findById(microcredentialId)
+            .orElseThrow(() -> new MicrocredentialNotFoundException(microcredentialId));
+
+    microcredentialEntity.setStatus(
+        approved ? MicrocredentialStatus.GRANTED : MicrocredentialStatus.REJECTED);
+
+    if (approved) {
+      microcredentialEntity.setAssignmentDate(LocalDate.now());
+    }
+
+    final var microcredentialSavedEntity =
+        this.microcredentialSpringData.save(microcredentialEntity);
+    return this.microcredentialMapper.toDomain(microcredentialSavedEntity);
   }
 
   @Override
   public List<Microcredential> findPending() {
-    // TODO: Complete the implementation
-    return null;
-  }
 
-  @Override
-  public List<Microcredential> findByCourseId(Long courseId) {
-    // TODO: Complete the implementation
-    return null;
+    return this.microcredentialSpringData.findByStatus(MicrocredentialStatus.REQUESTED).stream()
+        .map(this.microcredentialMapper::toDomain)
+        .collect(Collectors.toList());
   }
 }
